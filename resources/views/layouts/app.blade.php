ed356712859ae3e638cd464910ccd48a24f11a8d
jha.kuldip <jhakuldip40@gmail.com>
2024-08-08 19:27:27 +0530

    improved Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Product Management')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-4">
         @yield('content')
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,18 +2,31 @@
 
 @section('content')
     <h1>Products</h1>
-    <a href="{{ route('products.create') }}">Create New Product</a>
-    <ul>
-        @foreach($products as $product)
-            <li>
-                <a href="{{ route('products.show', $product['id']) }}">{{ $product['title'] }}</a>
-                <a href="{{ route('products.edit', $product['id']) }}">Edit</a>
-                <form action="{{ route('products.destroy', $product['id']) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
+    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">Create New Product</a>
+    
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{ $product['id'] }}</td>
+                    <td><a href="{{ route('products.show', $product['id']) }}">{{ $product['title'] }}</a></td>
+                    <td>
+                        <a href="{{ route('products.edit', $product['id']) }}" class="btn btn-warning btn-sm">Edit</a>
+                        <form action="{{ route('products.destroy', $product['id']) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
